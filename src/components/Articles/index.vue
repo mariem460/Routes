6a51cf4f22ea784ab087d2eb3e16bdd3cbd0fb06
mariem460 <mariem.oueslati.hajjem@gmail.com>
2022-09-06 12:35:10 +0200

    params

--- src/components/Articles/index.vue
+++ src/components/Articles/index.vue
@@ -1,139 +1,17 @@
 <template>
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" v-for="article in articles" :key="article.id">
     <!--  ITEM -->
     <div class="col">
         <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
+            <img class="bd-placeholder-img card-img-top" :src="article.img+ '?' + Math.random() "/>
             <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <p>{{article.excerpt}}</p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                        <router-link to="/articles/2">
+                        <router-link to="'/articles/' + article.id">
                             <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
 
                         </router-link>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                     </div>
                 </div>
             </div>
@@ -143,4 +21,22 @@
 
 </div>
 </template>
+
+
+<script>
+    import axios from 'axios'
+   export default {
+    data() {
+        return {
+            articles: []
+
+        }
+    },
+    mounted() {
+        axios.get('http://localhost:3000/articles')
+        .then((response)=> 
+        this.articles= response.data)
+    }
+   }
+</script>
 
